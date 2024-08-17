e4fe98a2efb9ccb63d76a29d14267935699f6a53
Riddlerjoke <jeremy.lardic@hotmail.fr>
2024-08-17 17:08:51 +0200

    first test deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/react-github-page/app",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
