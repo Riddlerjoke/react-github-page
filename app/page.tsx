a74ce723e95d0ba3cf5f81d867609c7f2c690913
Riddlerjoke <jeremy.lardic@hotmail.fr>
2024-08-18 18:52:19 +0200

    mise en page landing

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex bg-gray-500">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-500">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex ">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-r from-blue-400 to-emerald-600">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
